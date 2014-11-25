5b74a48a0a6acad4df3649ac4146f26ec86b8ee1
2hansen <bo@2hansen.dk>
2014-11-25 14:28:37 +0100

    more fun

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 function install_nginx {
+	echo "### Install nginx"
 	sudo apt-get install -y nginx
 	cp /vagrant/vagrant/default /etc/nginx/sites-enabled
-	/etc/init.d/nginx restart
+	/etc/init.d/nginx restart	
 }
 
-function install_nodejs {
-	apt-get install -y nodejs-legacy npm
+function install_devtools {
+	echo "### Install nodejs and dev tools"
+	apt-get install -y nodejs-legacy npm git
+	cd /vagrant
+	npm install -g bower
+	npm install karma
+	npm install -g karma-cli
+	npm install phantomjs
+	bower install angular
+	bower install bootstrap
+	cd
 }
 
+echo "### Bootstrappin'!"
 sudo apt-get update
-sudo apt-get install -y language-pack-en
 install_nginx
-install_nodejs
+install_devtools
+
+echo "### All done!"
