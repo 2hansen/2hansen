68afbb8d1dd74ef0b1d85aabf51e2f95b7a46a2b
2hansen <bo@2hansen.dk>
2014-12-15 15:39:24 +0100

    Begin using route

--- app/app.js
+++ app/app.js
@@ -1,6 +1,19 @@
 'use strict';
 
-var app = angular.module("staffApp", ['nvd3', 'staffControllers', 'staffServices'])
+var app = angular.module("staffApp", ['nvd3', 'ngRoute', 'staffControllers', 'staffServices'])
+
+nv.dev = false;
+
+
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+		when('/', {
+			templateUrl: 'staff.html'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+}]);
 	/*.run(function($rootScope){
 		$rootScope.employees = [];
 	})*/
