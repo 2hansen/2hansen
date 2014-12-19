ef4e2315d0ff7054986a2d0a6398446f6d3f6b40
Bo Hansen Hansen <bhh@A1419-iMac.local>
2014-12-19 15:08:59 +0100

    Fix the Employee scope variable

--- app/controllers/employee-controllers.js
+++ app/controllers/employee-controllers.js
@@ -4,20 +4,33 @@ var employeeControllers = angular.module('employeeControllers', []);
 employeeControllers.controller('employeeCtrl', ['$scope', '$routeParams', '$log', 'Employees', function($scope, $routeParams, $log, Employees) {
 	$scope.employeeId = $routeParams.employeeId;	
 	$scope.employees = Employees.query();
-
+	$scope.employees.$promise.then(function(employees){
+		for(var employee in employees){
+			if(employees[employee]._id == $routeParams.employeeId){
+				$scope.employee = employees[employee];	
+				break;
+			}
+		}	
+	});
 }]);
 
+/*var User = $resource('/user/:userId', {userId:'@id'});
+User.get({userId:123})
+    .$promise.then(function(user) {
+      $scope.user = user;
+    });*/
+
+
 //The profileController
-employeeControllers.controller('profileCtrl', ['$scope', '$log', 'Employee', function($scope, $log, Employee) {
+employeeControllers.controller('profileCtrl', ['$scope', '$log', function($scope, $log) {
 	//$log.log($scope.employee.name.first);
-	$scope.employee = Employee.get();
+	//$scope.employee = Employee.get();
 
 }]);
 
 //The skillController
 employeeControllers.controller('skillCtrl', ['$scope', '$routeParams', '$log', 'Joke', 'Employees', function($scope, $routeParams, $log, Joke, Employees) {
-	$scope.employees.$promise.then(function(employees){
-		$scope.employee = Employees//var theEmployee = {};
+/*	$scope.employees.$promise.then(function(employees){
 		for(var employee in employees){
 				if(employees[employee]._id == $routeParams.employeeId){
 				$scope.joke = Joke.get({firstName: employees[employee].name.first, lastName: employees[employee].name.last}, function(joke) {
@@ -31,5 +44,16 @@ employeeControllers.controller('skillCtrl', ['$scope', '$routeParams', '$log', '
 				break;
 			}
 		}
+	});*/
+
+	$scope.employees.$promise.then(function(employees){
+		$scope.joke = Joke.get({firstName: $scope.employee.name.first, lastName: $scope.employee.name.last}, function(joke) {
+		    if($scope.employee.sex == 'female'){
+				$scope.skills = unescape(joke.value.joke.replace(/ he /g, " she ").replace(/ his /g, " her ").replace(/ He /g, " She ").replace(/ him /g, " her ").replace(/ He'/g, " She'").replace(/he'/g, "she'").replace(/&quot;/g, "\""));
+			}
+			else{
+				$scope.skills = joke.value.joke;
+			}
+		});
 	});
 }]);
--- app/services/employees-service.js
+++ app/services/employees-service.js
@@ -5,27 +5,3 @@ employeesService.factory('Employees', ['$resource', function($resource){
       query: {method:'GET', /*params:{phoneId:'phones'},*/ isArray:true}
     });
   }]);
-
-employeesService.factory('Employee', ['Employees', function(Employees){
-	//var theEmployee = {};
-	var employees = Employees.query();
-	
-	return{
-		get: function(){
-			var emp;
-			employees.$promise.then(function(employees){
-				for(var employee in employees){
-					if(employees[employee]._id == '5492d60344fcd945f4df63c1'){
-						emp = employees[employee]		
-						break;
-					}
-				}
-				return emp;	
-			});
-		},
-		set: function(){
-
-		}
-	}
-}]);
-
